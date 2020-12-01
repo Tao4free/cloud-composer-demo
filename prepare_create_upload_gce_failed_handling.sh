f07deea26e3d74f22a8ec65e6629a280a0146ef4
tao4free <tao4free@gmail.com>
2020-12-02 02:05:29 +0900

    add failed handling prepare bash

---
+++ prepare_create_upload_gce_failed_handling.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+source config.sh
+
+gcloud composer environments storage dags import \
+  --environment ${COMPOSER_ENV_NAME} \
+  --location ${LOCATION} \
+  --source create_upload_gcs_failed_handling.py
