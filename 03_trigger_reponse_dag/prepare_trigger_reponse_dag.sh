67c9e015eabec8e54ef2a5b0cd9f13634e1ca12f
tao4free <tao4free@gmail.com>
2020-12-02 11:07:30 +0900

    modify to run properly

--- 03_trigger_reponse_dag/prepare_trigger_reponse_dag.sh
+++ 03_trigger_reponse_dag/prepare_trigger_reponse_dag.sh
@@ -5,11 +5,12 @@ webserver_res=`gcloud composer environments describe ${COMPOSER_ENV_NAME} --loca
 webserver_id=${webserver_res##*' '}
 client_id=`python3 get_client_id.py ${PROJECT_ID} ${LOCATION} ${COMPOSER_ENV_NAME}`
 
-gcloud functions deploy my_function \
-    --source=gcs-dag-trigger-function \
-    --runtime=python38 \ 
-    --trigger-bucket=${UPLOAD_GCS_BUCKET_NAME} \
-    --trigger-event=google.storage.object.finalize \
+gcloud functions deploy gcs-dag-trigger-function \
+    --runtime python38 \
+    --source gcs-dag-trigger-function \
+    --entry-point trigger_dag \
+    --trigger-resource ${UPLOAD_GCS_BUCKET_NAME} \
+    --trigger-event google.storage.object.finalize \
     --set-env-vars=CLIENT_ID=${client_id},TENANT_PROJECT=${webserver_id}
 
 gcloud composer environments storage dags import \
