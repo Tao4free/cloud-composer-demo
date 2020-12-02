7e4bb06b1f1595ca9937d9daa7845d310f2073c4
tao4free <tao4free@gmail.com>
2020-12-02 20:04:56 +0900

    modify for test

--- 04_clean_demo_set/clean_demo_set.sh
+++ 04_clean_demo_set/clean_demo_set.sh
@@ -2,11 +2,12 @@
 source ../config.sh
 
 composer_gcs_entry=`gcloud composer environments describe ${COMPOSER_ENV_NAME} --location ${LOCATION} | grep dagGcsPrefix`
-composer_gcs_path=${webserver_res##*' '}
+composer_gcs_path=${composer_gcs_entry##*' '}
+composer_gcs_path=${composer_gcs_path/\/dags//}
 
-gcloud composer environments delete ${COMPOSER_ENV_NAME}
+gcloud composer environments delete ${COMPOSER_ENV_NAME} --location ${LOCATION} --quiet
 
-gcloud functions delete gcs-dag-trigger-function
+gcloud functions delete gcs-dag-trigger-function --quiet
 
-gsutil rm -r gs://${composer_gcs_path}
-gsutil rm -r gs://${UPLOAD_GCS_BUCKET_NAME}
+gsutil rb  ${composer_gcs_path}
+gsutil rb  gs://${UPLOAD_GCS_BUCKET_NAME}
