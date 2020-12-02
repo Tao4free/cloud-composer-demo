adb0b86597c1b95ef85bbc91f127843b69a26c91
tao4free <tao4free@gmail.com>
2020-12-02 10:32:17 +0900

    arrage folder

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+PROJECT_ID=ca-willsbooster-test
 LOCATION=asia-northeast1
 ZONE=asia-northeast1-b
 UPLOAD_GCS_BUCKET_NAME=test-composer-uploaded-data
---
+++ trigger_reponse_dag/prepare_trigger_reponse_dag.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+source config.sh
+
+webserver_res=`gcloud composer environments describe ${COMPOSER_ENV_NAME} --location ${LOCATION} | grep airflowUri`
+webserver_id=${webserver_res##*' '}
+client_id=`python3 get_client_id.py ${PROJECT_ID} ${LOCATION} ${COMPOSER_ENV_NAME}`
+
+gcloud functions deploy my_function \
+    --source=gcs-dag-trigger-function \
+    --runtime=python38 \ 
+    --trigger-bucket=${UPLOAD_GCS_BUCKET_NAME} \
+    --trigger-event=google.storage.object.finalize \
+    --set-env-vars=CLIENT_ID=${client_id},TENANT_PROJECT=${webserver_id}
+
+gcloud composer environments storage dags import \
+  --environment ${COMPOSER_ENV_NAME} \
+  --location ${LOCATION} \
+  --source trigger_reponse_dag.py
