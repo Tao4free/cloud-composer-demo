ab1fa4c6569c4af56793bc407cf5f6e5d266b604
Tao4free <29458813+Tao4free@users.noreply.github.com>
2020-12-02 11:18:57 +0900

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-PROJECT_ID=ca-willsbooster-test
+PROJECT_ID=xxx
 LOCATION=asia-northeast1
 ZONE=asia-northeast1-b
-UPLOAD_GCS_BUCKET_NAME=test-composer-uploaded-data
-COMPOSER_ENV_NAME=wills-composer-demo
+UPLOAD_GCS_BUCKET_NAME=xxx
+COMPOSER_ENV_NAME=xxx
 
