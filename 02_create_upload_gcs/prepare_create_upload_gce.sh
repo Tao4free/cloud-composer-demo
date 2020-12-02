14aa9d4f72a02c2643c7f187e8f0516b0b42d7ae
tao4free <tao4free@gmail.com>
2020-12-02 18:05:40 +0900

    test to load loack package to composer

--- 02_create_upload_gcs/prepare_create_upload_gce.sh
+++ 02_create_upload_gcs/prepare_create_upload_gce.sh
@@ -3,13 +3,18 @@ source ../config.sh
 
 gsutil mb gs://${UPLOAD_GCS_BUCKET_NAME}
 
-gcloud composer environments update ${COMPOSER_ENV_NAME} \
-    --location ${LOCATION} \
-    --update-pypi-packages-from-file requirements.txt || true
+# gcloud composer environments update ${COMPOSER_ENV_NAME} \
+    # --location ${LOCATION} \
+    # --update-pypi-packages-from-file requirements.txt || true
 
 gcloud composer environments update ${COMPOSER_ENV_NAME} \
     --location ${LOCATION} \
     --update-env-variables=UPLOAD_GCS_BUCKET_NAME=${UPLOAD_GCS_BUCKET_NAME} || true
+
+gcloud composer environments storage dags import \
+  --environment ${COMPOSER_ENV_NAME} \
+  --location ${LOCATION} \
+  --source pendulum
 
 gcloud composer environments storage dags import \
   --environment ${COMPOSER_ENV_NAME} \
